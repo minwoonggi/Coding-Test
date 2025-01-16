b4943cf2e6e7076e2c97d5c62e40b3b42116fdab
민웅기 <150315193+minwoonggi@users.noreply.github.com>
2025-01-16 16:57:28 +0900

    [Silver II] Title: 비슷한 단어, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,43 +1,19 @@
+N = int(input())
+target = list(input()) # 비교 대상 단어(첫 단어)
+answer = 0
 
-n=int(input())
+for _ in range(N-1):
+    compare = target[:] 
+    word = input() # 새로운 단어
+    cnt = 0
 
-first_str = list(input())
-first_str.sort()
-cnt=0
-for i in range(n-1):
-  str = list(input())
-  str.sort()
-  if(str==first_str):
-    cnt+=1 
-  elif (len(str)==len(first_str)):
-    for i in range(len(str)):
-      correct = 0
-      for k in range(65,91):
-        str_copy = str.copy()
-        str_copy[i]=chr(k)
-        str_copy.sort()
-        if(str_copy==first_str):
-          cnt+=1
-          correct=1
-      if(correct==1):
-        break
-  elif (len(str)+1==len(first_str)):
-    for k in range(65,92):
-      str_copy = str.copy()
-      str_copy.append(chr(k))
-      str_copy.sort()
-      if(str_copy==first_str):
-          cnt+=1
-          break
-  elif(len(str)-1==len(first_str)):
-    for k in range(len(str)):
-      str_copy = str.copy()
-      str_copy.pop(k)
-      str_copy.sort()
-      if(str_copy==first_str):
-        cnt+=1
-        break
-print(cnt)
-      
-      
-      
+    for w in word:
+        if w in compare:
+            compare.remove(w)
+        else:
+            cnt += 1
+
+    if cnt < 2 and len(compare) < 2:
+        answer += 1
+
+print(answer)
