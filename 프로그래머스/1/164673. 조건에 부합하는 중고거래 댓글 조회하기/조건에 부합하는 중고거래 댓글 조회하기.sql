179e7d75c85136048ace09335d88a9299f390c64
민웅기 <150315193+minwoonggi@users.noreply.github.com>
2025-06-17 22:01:20 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,12 +1,2 @@
-SELECT 
-    ugb.TITLE, 
-    ugb.BOARD_ID, 
-    ugr.REPLY_ID, 
-    ugr.WRITER_ID, 
-    ugr.CONTENTS, 
-    DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
-FROM USED_GOODS_BOARD ugb 
-JOIN USED_GOODS_REPLY ugr ON ugb.BOARD_ID = ugr.BOARD_ID 
-WHERE YEAR(ugb.CREATED_DATE) = 2022 
-    AND MONTH(ugb.CREATED_DATE) = 10
-ORDER BY ugr.CREATED_DATE ASC, ugb.TITLE ASC;
+SELECT TITLE, ugb.BOARD_ID,REPLY_ID,ugr.WRITER_ID,ugr.CONTENTS, DATE_FORMAT(ugr.created_date, '%Y-%m-%d') AS CREATED_DATE from USED_GOODS_BOARD ugb join used_goods_reply ugr on ugb.board_id = ugr.board_id 
+where ugb.created_date >= '2022-10-01' and ugb.created_date <'2022-11-01' order by ugr.created_date asc, ugb.title;
