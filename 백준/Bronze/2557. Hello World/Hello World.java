af8f11f7f82523510f9e14ba9546c85d5b80081e
민웅기 <150315193+minwoonggi@users.noreply.github.com>
2025-06-29 17:02:37 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args)  {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+        System.out.printf(sb.toString());
+    }
+}
