ddbf0800827e54c617404efcdadfc59b61aeb764
민웅기 <150315193+minwoonggi@users.noreply.github.com>
2025-08-03 15:17:31 +0900

    Fix: Update 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,2 +1,6 @@
-select count(*) as FISH_COUNT,max(LENGTH) as MAX_LENGTH, FISH_TYPE from FISH_INFO group by FISH_TYPE having avg(LENGTH)>=33 
-order by FISH_TYPE ; 
+select count(*) as FISH_COUNT,max(t.LENGTH) as MAX_LENGTH, t.FISH_TYPE from 
+(select CASE WHEN (LENGTH is null or LENGTH <=10) then 10 
+ else LENGTH end as "LENGTH", ID,FISH_TYPE	,TIME
+ from FISH_INFO) as t 
+group by t.FISH_TYPE having avg(t.LENGTH)>=33 
+order by t.FISH_TYPE ; 
